a7a49ac3d961eda2e41c9326ebfafcb88a7ecfc8
Vishesh Kumar <vk7879@srmist.edu.in>
2022-04-13 03:41:34 +0530

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -70,89 +71,117 @@ class _HomePageState extends State<HomePage> {
             Expanded(
               child: Row(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 450,
-                        child: Text(
-                          "Lorem Ipusm dol solor olasa random",
-                          style: GoogleFonts.poppins(
-                            fontSize: 38,
-                            fontWeight: FontWeight.w700,
-                            color: LawAidColor.primaryColor,
-                          ),
-                        ),
+                Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 450,
+                    child: Text(
+                      "Lorem Ipusm dol solor olasa random",
+                      style: GoogleFonts.poppins(
+                        fontSize: 38,
+                        fontWeight: FontWeight.w700,
+                        color: LawAidColor.primaryColor,
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        width: 450,
-                        child: Text(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                          style: GoogleFonts.poppins(
-                              color: LawAidColor.textColor,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 16),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      MouseRegion(
-                        child: Container(
-                          height: 50,
-                          width: 400,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(15),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Color(0xff39230014),
-                                    offset: Offset(0, 2),
-                                    blurRadius: 60)
-                              ]),
-                          child: TextField(
-                            controller: serachText,
-                            decoration: InputDecoration(
-                              filled: true,
-                              hintText: "Search for...",
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.transparent),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.transparent),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(height: 10),
-                      Container(height: MediaQuery.of(context).size.height*.8,child: Image.asset('images/Justice-pana.png'))
-                    ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 450,
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                      style: GoogleFonts.poppins(
+                          color: LawAidColor.textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  MouseRegion(
+                    child: Container(
+                      height: 50,
+                      width: 400,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xff39230014),
+                                offset: Offset(0, 2),
+                                blurRadius: 60)
+                          ]),
+                      child: TextField(
+                        controller: serachText,
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: "Search for...",
+
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                  Container(
+                    child: Column(
+                        children: [
+                        Text("Lorem Ipsum"),
+                    SizedBox(
+                      height: 12),
+                  MaterialButton(onPressed: () {},
+
                   )
                 ],
               ),
             )
           ],
         ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 10),
+            Container(height: MediaQuery
+                .of(context)
+                .size
+                .height * .7, width: MediaQuery
+                .of(context)
+                .size
+                .height * 1.3, child: Image.asset('images/Justice-pana.png'))
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          ],
+
+        )
+        ],
       ),
+    )],
+    )
+    ,
+    )
+    ,
     );
   }
 }
